0f3bd3103873abf1835c92971c0275fcc1637550
Arda Erturk <arda.erturk@mail.utoronto.ca>
2024-03-13 22:01:24 -0400

    replicate walletClient issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 "use client";
 
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
@@ -7,11 +8,107 @@ import {
   DynamicWidget,
 } from "@dynamic-labs/sdk-react-core";
 
+import usdtAbi from "../public/usdtAbi.json";
+
+import { http, createPublicClient } from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { polygon } from "viem/chains";
+
+import { LocalAccountSigner } from "@alchemy/aa-core";
+
+import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
+
+import {
+  signerToSessionKeyValidator,
+  ParamOperator, oneAddress
+} from "@zerodev/session-key";
+import { walletClientToSmartAccountSigner } from "permissionless";
+import { createKernelAccount } from "@zerodev/sdk";
+
 export default function Home() {
   return (
     <DynamicContextProvider
       settings={{
-        environmentId: "b93dc1e8-974f-479b-92b5-eafa7daf92de",
+        environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID,
+        eventsCallbacks: {
+          onAuthSuccess: async (args) => {
+
+              const publicClient = createPublicClient({
+                chain: polygon,
+                transport: http(process.env.NEXT_PUBLIC_ZERODEV_BUNDLER_RPC),
+              });
+
+              const sessionPrivateKey = generatePrivateKey();
+              const sessionKeySigner = privateKeyToAccount(sessionPrivateKey);
+
+              const owner =
+                LocalAccountSigner.privateKeyToAccountSigner(sessionPrivateKey);
+
+
+
+              const dynamicWalletClient: any =
+                await args?.primaryWallet?.connector?.getWalletClient();
+
+
+
+              const smartAccountSigner = await walletClientToSmartAccountSigner(
+                dynamicWalletClient
+              );
+
+
+
+              const ecdsaValidator = await signerToEcdsaValidator(
+                publicClient,
+                {
+                  signer: smartAccountSigner,
+                }
+              );
+
+              const permissions = [
+                {
+                  target: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+                  valueLimit: 0n,
+                  abi: usdtAbi,
+                  functionName: "balanceOf",
+                  args: [
+                    {
+                      operator: ParamOperator.EQUAL,
+                      value: args?.primaryWallet?.address,
+                    }, // User's address
+                  ],
+                },
+                {
+                  target: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f",
+                  valueLimit: 0n,
+                  abi: usdtAbi,
+                  functionName: "transfer",
+                  args: [{ operator: ParamOperator.GREATER_THAN, value: 0 }],
+                },
+              ];
+
+
+              const sessionKeyValidator = await signerToSessionKeyValidator(
+                publicClient,
+                {
+                  signer: sessionKeySigner,
+                  validatorData: {
+                    paymaster: oneAddress,
+                    permissions: permissions,
+                  },
+                }
+              );
+
+              const sessionKeyAccount = await createKernelAccount(
+                publicClient,
+                {
+                  plugins: {
+                    sudo: ecdsaValidator,
+                    regular: sessionKeyValidator,
+                  },
+                }
+              );
+          },
+        },
         walletConnectors: [
           EthereumWalletConnectors,
           ZeroDevSmartWalletConnectors,
